1d4100b0dd875d4069e478dd21cbe8d394229adc
amrithm98 <amrithchallenger@gmail.com>
2018-05-15 16:04:45 +0530

    Reg Working

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -9,17 +9,17 @@ app.controller('regController', ['$scope', '$http', function($scope, $http) {
 
     $scope.register = function() {
         console.log("ANGULAR");
-        var url = 'http://139.59.79.82:3030/register';
+        var url = 'http://localhost:3030/register';
         console.log($scope.data);
-        // $http.post(url, $scope.data).then(function(success) {
-        //     var resp = success.data;
-        //     // console.log(resp);
-        //     $scope.status = resp.msg;
-        //     alert(resp.msg);
-        //     $scope.data = {};
-        // }, function(err) {
-        //     // console.log(err);
-        // });
+        $http.post(url, $scope.data).then(function(success) {
+            var resp = success.data;
+            // console.log(resp);
+            $scope.status = resp.msg;
+            alert(resp.msg);
+            $scope.data = {};
+        }, function(err) {
+            // console.log(err);
+        });
     };
 
 }]);
